fca4dfadf3d4fd23b0a923640cd73555c5cef4b7
medinamaria90@gmail.com <medinamaria90@gmail.com>
2023-12-17 18:48:41 +0100

    Changes at home

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,192 +6,128 @@
 /*   By: marimedi <marimedi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/11 16:16:36 by marimedi          #+#    #+#             */
-/*   Updated: 2023/12/15 16:11:29 by marimedi         ###   ########.fr       */
+/*   Updated: 2023/12/17 00:34:56 by marimedi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-
 #include "get_next_line.h"
-#include <string.h>
 
-char	*ft_strdup(const char *s)
+char	*free_memory(char *str)
+{
+	free (str);
+	str = NULL;
+	return (NULL);
+}
+
+char	*ft_strdup_len(const char *s, int len)
 {
 	char	*str;
-	size_t	size;
+	int		size;	
 
-	size = ft_strlen(s) + 1;
+	if (s == NULL)
+		return (NULL);
+	if ((len == -1) || ((int)ft_strlen(s) < len))
+		size = ft_strlen(s) + 1;
+	else
+		size = len;
 	str = (char *)malloc(size * sizeof(char));
-	if (str == NULL)
+	if (!str)
 		return (NULL);
 	ft_strlcpy(str, s, size);
 	return (str);
 }
 
-char *ft_divide_line(char **old_buffer)
+char	*ft_divide_line(char **container)
 {
-	char		*aux_buff;
-	char		*new_line;
-	size_t		new_line_pos;
-	size_t		buff_len;
-	size_t		old_buffer_len;
-	
-	buff_len = ft_strlen(*old_buffer);
-	if (buff_len == 0)
-		return (0);
-	new_line_pos = ft_strchr(*old_buffer, '\n') - *old_buffer;
- 	if (new_line_pos > buff_len) 
-    	return(NULL);
-	else
+	char	*aux_buff;
+	char	*new_line;
+	size_t	break_line;
+
+	if (ft_strchr(*container, '\n') == NULL)
 	{
-		new_line = (char *)malloc(sizeof(char) * (new_line_pos) + 1);
-		ft_strlcpy(new_line, (*old_buffer), new_line_pos + 1); 
-		old_buffer_len = ft_strlen(&(*old_buffer)[new_line_pos + 1]);
-		aux_buff = (char *)malloc(sizeof(char) * (old_buffer_len + 1));
-		ft_strlcpy(aux_buff, &(*old_buffer)[new_line_pos + 1], old_buffer_len + 1);
-		free (*old_buffer);
-		*old_buffer = (char *)malloc(sizeof(char) * (old_buffer_len + 1));
-		ft_strlcpy(*old_buffer, aux_buff, old_buffer_len + 1);
-		//printf("Temp buff %s\n", *old_buffer);
-		free (aux_buff);
+		new_line = ft_strdup_len(*container, -1);
+		if (!new_line)
+			return (NULL);
+		free(*container);
+		*container = NULL;
 		return (new_line);
 	}
-	return (NULL);
+	break_line = (ft_strchr(*container, '\n') - *container) + 1;
+	aux_buff = ft_strdup_len(&(*container)[break_line], -1);
+	if (!aux_buff)
+		return (NULL);
+	new_line = ft_strdup_len((*container), break_line + 1);
+	if (!new_line)
+		free_memory(aux_buff);
+	free (*container);
+	*container = aux_buff;
+	return (new_line);
 }
 
-char *ft_read_line(char *buffer, int bytes, char **old_buffer)
-{
-	char		*line;
-	char		*new_buffer;
-	char		*joint_buffer;
-	size_t		old_buffer_size;
-	static	int	written;
-	//printf("at the beggining of read line\n");
-	//printf("Beg Buffer -- %s\nBeg Old Buffer -- %s\n", buffer, *old_buffer);
-	old_buffer_size = ft_strlen(*old_buffer);
-	if (bytes == 0)
-	{
-		if (written == 0)
-			return (NULL);
-		if (old_buffer_size == 0)
-			return (ft_strdup(""));
-		return(strdup(*old_buffer));
-	}
-	written = 1;
-	new_buffer = (char *)malloc(sizeof(char) * (bytes + 1));
-	ft_strlcpy(new_buffer, buffer, bytes + 1);
-	if (old_buffer_size > 0)
-	{
-		joint_buffer = ft_strjoin((char const *)*old_buffer, (char const *)new_buffer);
-		free(*old_buffer);
-		*old_buffer = ft_strdup(joint_buffer); 
-		free(joint_buffer);
-	}
-	else
-	{
-		*old_buffer = ft_strdup(new_buffer);
-		free(new_buffer);
-	}
-	//printf("AFTER Old Buffer -- %s\n", *old_buffer);
-	if (ft_strchr(*old_buffer, '\n'))
-	{
-		line = ft_divide_line(old_buffer);	
-		//printf("YESS NEW LINE -- %s\n", line);
-		//printf("Old buffer is -- %s\n", *old_buffer);
-
-		return(line);
-	}
-	return(NULL);
-}
-
-char *ft_read_file(int fd, char *buffer, size_t buff_size)
-{
-	int	bytes_read;
-	static int	end_of_file;
-	char	*line;
-	static char	*old_buffer;
-
-	if (end_of_file == 1)
-	{
-		return(NULL);
-	}
-	//printf("Before the line\n");
-	line = ft_divide_line(&old_buffer);
-	if (line != NULL)
-		return (line);
-	bytes_read = 1;
-	//printf("Buffer is --> %s\n", buffer);
-	while ((bytes_read = read(fd, buffer, buff_size)) > 0)
-	{
-		//bytes_read = read(fd, buffer, buff_size);
-		//printf("The buffer is %s\n", buffer);
-		//printf("The old buffer is %s\n", old_buffer);
-		//printf("Bytes read are %i\n", bytes_read);
-		if (bytes_read == -1)
-		{
-			free(old_buffer); 
-			//printf("bytes read were -1\n");
-			return (NULL);
-		}
-		if (ft_strchr(buffer, '\n'))
-		{
-			line = ft_read_line(buffer, bytes_read, &old_buffer);
-			//printf("buffer and bytes read %s, %d\n", buffer, bytes_read);
-			//memset((void *)buffer, 0, bytes_read);
-			return(line);
-		}
-		line = ft_read_line(buffer, bytes_read, &old_buffer);
-		//printf("Line is %s\n", line);
-		if (line != NULL)
-			return(line);
-		//printf("buffer is %s\n", buffer);
-		//memset((void *)buffer, 0, bytes_read);
-	}
-	if (bytes_read == 0)
-	{
-		//printf("temp buff is %s", old_buffer);
-		//printf("bytes read == 0\n");
-		/*
-		line = malloc(sizeof(char) * (ft_strlen(old_buffer) + 1));
-		if (line == NULL)
-			free(old_buffer);
-		ft_strlcpy(line, old_buffer, (ft_strlen(old_buffer) + 1));
-		
-		*/
-		line = ft_read_line(buffer, 0, &old_buffer);
-		free(old_buffer);
-		end_of_file = 1;
-		return (line);
-	}
-	return (line);
-}
-
-char *get_next_line(int fd)
+char	*read_file(int fd, char **container)
 {
 	char	*buffer;
-	size_t	buff_size;
-	char	*line;
-	
-	if (fd == -1)
-		return (NULL);
-    #ifdef BUFFER_SIZE
-        buff_size = BUFFER_SIZE;
-    #else
-        buff_size = DEFAULT_BUFFER_SIZE;
-    #endif
-	buffer = (char *)malloc(sizeof(char) * (buff_size + 1));
-	buffer[0] = 0;
+	char	*temp;
+	int		bytes;
+
+	buffer = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
 	if (buffer == NULL)
 		return (NULL);
-	//printf("Buffer before sending is -> %s\n", buffer);
-	line = ft_read_file(fd, buffer, buff_size);
-	//printf("line is %s\n", line);
-	free(buffer);
+	bytes = 1;
+	buffer[0] = 0;
+	while (!ft_strchr(buffer, '\n') && bytes > 0)
+	{
+		bytes = read(fd, buffer, BUFFER_SIZE);
+		//printf ("bts:%d ", bytes);
+		if (bytes > 0)
+		{
+			//printf("Buffer is --> %s ", buffer);
+			buffer[bytes] = '\0';
+			//printf("Buffer is --> %s ", buffer);
+			temp = ft_strjoin(*container, buffer);
+			if (!temp)
+				return (free_memory(buffer));
+			free(*container);
+			*container = temp;
+		}
+	}
+	if (bytes == -1)
+		return (free_memory(buffer));
+	return (buffer);
+}
+
+char	*get_next_line(int fd)
+{
+	char		*line;
+	char		*buffer;
+	static char	*container;
+
+	if (fd < 0 || BUFFER_SIZE < 1 || read(fd, 0, 0) < 0)
+		return (NULL);
+	buffer = read_file(fd, &container);
+	if (!buffer)
+	{
+		printf("buffer is null\n");
+		return (free_memory(container));
+	}
+	if (!container)
+		return (free_memory(buffer));
+	line = ft_divide_line(&container);
+	if (!line)
+		return (free_memory(buffer));
+	free (buffer);
+	if (ft_strlen(line) == 0 && !container)
+	{
+		free(container);
+		return (free_memory(line));
+	}
 	return (line);
 }
+
 /*
 int main(void) 
 {
-    int file_descriptor = open("testing_1_char.txt", O_RDONLY);
+    int file_descriptor = open("testing1.txt", O_RDONLY);
     if (file_descriptor == -1) 
 	{
         perror("Error opening file");
@@ -199,10 +135,10 @@ int main(void)
     }
 
     char *line;
-	int	line_number = 0;
+	int	line_number = 1;
 
-    // Read lines until the end of the file
-    while ((line = get_next_line(file_descriptor)) != NULL) 
+    while ((line = get_next_line(file_descriptor)) != NULL//
+	 && (line_number < 15)) 
 	{
         printf("Line %d: %s\n", line_number, line);
         line_number++;
--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -1,20 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marimedi <marimedi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/16 17:53:56 by marimedi          #+#    #+#             */
+/*   Updated: 2023/12/16 17:57:17 by marimedi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-
-#include <stdlib.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <stdio.h>
-
-#ifndef BUFFER_SIZE
-# define DEFAULT_BUFFER_SIZE 42
-#endif
-
+# include <stdlib.h>
+# include <unistd.h>
+# include <fcntl.h>
+# include <stdio.h>
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
 
 char	*ft_strjoin(char const *s1, char const *s2);
 size_t	ft_strlen(const char *str);
 size_t	ft_strlcpy(char *dest, const char *src, size_t n);
 char	*ft_strchr(const char *s, int c);
 char	*get_next_line(int fd);
-
 #endif
