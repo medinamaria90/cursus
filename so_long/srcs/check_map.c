6971b1de7972312a4fc1dd5499a87f315c4553b5
Maria Medina Canete <marimedi@c3r5s4.42malaga.com>
2024-02-16 18:32:37 +0100

    changes

--- so_long/srcs/check_map.c
+++ so_long/srcs/check_map.c
@@ -6,7 +6,7 @@
 /*   By: marimedi <marimedi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/12 14:28:38 by marimedi          #+#    #+#             */
-/*   Updated: 2024/02/15 14:02:49 by marimedi         ###   ########.fr       */
+/*   Updated: 2024/02/16 18:30:58 by marimedi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -92,7 +92,7 @@ void	go_every_pos(t_map *map)
 {
 	printf("Go to every pos");
 	map->visited[map->player.col][map->player.row] = '1';
-	if (map->grid[map->player.row++][map->player.col] != '1');
+	if (map->grid[map->player.row++][map->player.col] != '1')
 		map->visited[map->player.col][map->player.row] = '1';	
 	if (map->grid[map->player.row--][map->player.col] != '1')
 		map->visited[map->player.col][map->player.row] = '1';	
@@ -101,48 +101,62 @@ void	go_every_pos(t_map *map)
 	if (map->grid[map->player.col++][map->player.col] != '1')
 		map->visited[map->player.col][map->player.row] = '1';
 }
-int	get_new_pos(t_map *map, t_pos player)
+int	get_new_pos(t_map *map, t_pos *player)
 {
-	if (map->grid[player.row++][player.col] != 1 && map->visited[player.row++][player.col] != '1')
+	if (map->visited[player->row][player->col] == '1')
 	{
-		player.row++;
-		return (1);
+		printf("Already visited\n");
+		return (0);	
 	}
-	else if (map->grid[player.row--][player.col] != 1 && map->visited[player.row--][player.col] != '1')
+	map->visited[player->row][player->col] = '1';
+	if (map->grid[player->row + 1][player->col] != '1' && map->visited[player->row + 1][player->col] != '1')
 	{
-		player.row--;
-		return (1);
+		printf("current box content --> %c\n", map->grid[player->row][player->col]);
+		printf("next box content --> %c\n", map->grid[player->row + 1][player->col]);
+		player->row++;
+		printf("Go down\n");
 	}
-	else if (map->grid[player.col++][player.col] != 1 && map->visited[player.row][player.col++] != '1')
+	else if (map->grid[player->row - 1][player->col] != '1' && map->visited[player->row - 1][player->col] != '1')
 	{
-		player.col++;
-		return(1);
+		printf("current box visited --> %c\n", map->visited[player->row][player->col]);
+		printf("next box visited --> %c\n", map->visited[player->row - 1][player->col]);
+		printf("Go up\n");
+		player->row--;
 	}
-	else if (map->grid[player.col--][player.col] != 1 && map->visited[player.row][player.col--] != '1')
+	else if (map->grid[player->row][player->col + 1] != '1' && map->visited[player->row][player->col + 1] != '1')
 	{
-		player.col--;
-		return (1);
+		printf("next box content --> %c\n", map->grid[player->row][player->col + 1]);
+		printf("Go right\n");
+		player->col++;
 	}
-	printf("Returning 1\n");
-	return (0);
+	else if (map->grid[player->row][player->col - 1] != '1' && map->visited[player->row][player->col -1] != '1')
+	{
+		printf("Go left \n");
+		player->col--;
+	}
+	else
+	{
+		printf("No more movements\n");
+		return (0);
+	}
+	
+	return (1);
 }
 
 int walk_around(t_map *map, t_pos player) 
 {
 	t_pos	player_cpy;
 	int		new_pos;
-
-	printf("Starting\n");
+	
 	player_cpy.row = map->player.row;
 	player_cpy.col = map->player.col;
-	new_pos = get_new_pos(map, player);
-	printf("New pos is %d\n", new_pos);
-	while (new_pos == 1)
+	printf("player pos: row %d col %d\n", player.row, player.col);
+	while (get_new_pos(map, &player) == 1)
 	{
 		walk_around(map, player);
-		new_pos = get_new_pos(map, player_cpy);
+		printf("\nplayer NEW pos: row %d col %d\n", player.row, player.col);
+		new_pos = get_new_pos(map, &player);
 	}
-	print_map(map->visited);
 	return (0);
 }
 
@@ -150,23 +164,23 @@ int	initialize_visited(t_map *map)
 {
 	int	row;
 	int col;
-	
 	map->visited = malloc(map->rows * sizeof(char *));
 	if (map->visited == NULL)
 		return(1);
 	row = 0;
-	map->grid[row][col] = 0;
 	while (row < map->rows)
 	{
-		map->visited[row] = malloc(map->cols * sizeof(char));
+		map->visited[row] = malloc((map->cols) * sizeof(char));
 		col = 0;
 		while(col < map->cols)
 		{
 			map->visited[row][col] = '0';
 			col++;
 		}
+		map->visited[row][col] = 0;
 		row++;
 	}
+	map->visited[++row] = NULL;
 	return (0);
 }
 
@@ -178,6 +192,7 @@ int	check_map(t_map *map)
 	if (check_horizontal_walls(map) == 1)
 		return (1);
 	initialize_visited(map);
-	if (walk_around(map, map->player) == 1);
-		return (1);		
+	walk_around(map, map->player);
+	print_map(map->visited);
+	return (0);
 }
